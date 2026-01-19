720b5ca1f66ff9494743760ab6a74c5c6b96bc5c
OSIS smakda <osissmakda0@gmail.com>
2026-01-19 05:25:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3376876479-c4b98",
+  "appId": "1:554940764045:web:15ed80389c383335b9f7c4",
+  "apiKey": "AIzaSyCLse5I1W-3HOKdoEI3b6xZu4ZPxVFRoR4",
+  "authDomain": "studio-3376876479-c4b98.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "554940764045"
+};
