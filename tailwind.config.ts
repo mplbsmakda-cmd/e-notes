10bd36066e384e6006814f5cbbafdb6a54f5c127
OSIS smakda <osissmakda0@gmail.com>
2026-01-19 06:58:23 +0000

    kembangkan  UI / UX TERBAIK Dark / Light / Auto Custom theme Typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,9 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['PT Sans', 'sans-serif'],
-        code: ['monospace'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Lora', 'serif'],
+        code: ['Source Code Pro', 'monospace'],
       },
       colors: {
         background: 'hsl(var(--background))',
